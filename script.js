81072a491c8305f8687ff6e83028e512b9902873
Tony Thompson <penguinprofession@icloud.com>
2018-04-02 10:15:02 -0400

    added a tags to youtube results

--- script.js
+++ script.js
@@ -7,30 +7,49 @@ $(`#searchBar`).submit(function (event) {
     let location = $(`#location`).val();
     console.log(search);
     handleVideos(search);
+    // handleImages(search);
     handleMeetups(search, location);
     // $(`#search-term`).val(``);
 });
 
 function populateResults(data) {
-    console.log(`data populated!`)
+    console.log(`youtube data populated!`)
+    console.log(data);
     $(`#searchResults`).empty();
     let html = "";
     for (let i = 0; i < data.items.length; i++) {
-        html += '<img src ="' + data.items[i].snippet.thumbnails.medium.url + '"/>';
+        // html += '<p>' + data.items[i].snippet.title + '</p>';
+        html += '<a href="https://www.youtube.com/watch?v=' + data.items[i].id.videoId + '" ><img src ="' + data.items[i].snippet.thumbnails.medium.url + '"/></a>';
     }
     $(`#searchResults`).prop('hidden', false);
     $(`#searchResults`).append(html);
 }
 
+// function populateResultsC(data) {
+//     console.log(`data populated!`)
+//     $(`#searchResults`).empty();
+//     let html = "";
+//     for (let i = 0; i < data.items.length; i++) {
+//         html += '<img src ="' + data.items[i].snippet.thumbnails.medium.url + '"/>';
+//     }
+//     $(`#searchResults`).prop('hidden', false);
+//     $(`#searchResults`).append(html);
+// }
+
+
 function populateResultsB(data) {
     $(`#searchResultsB`).empty();
     let html = "";
     for (let i = 0; i < data.length; i++) {
-        html += '<img src ="' + data[i].organizer.photo.photo_link + '"/>';
+        const photoLink = data[i].key_photo.photo_link;
+        html += '<img id="results" src ="'+ photoLink +'"/>';
+        // console.log(photoLink);
     }
+    // $("#results").wrap($('<a>', {
+    //     href: '/Content/pdf/' + data.pdf1
+    // }));
     $(`#searchResultsB`).prop('hidden', false);
     $(`#searchResultsB`).append(html);
-    console.log(`data populated!`)
 }
 
 function handleVideos(search) {
@@ -38,32 +57,52 @@ function handleVideos(search) {
         key: `AIzaSyCrVDceP1-KwRsIVi12ODPCwS2oSHe-_7k`,
         q: search,
         part: `snippet`,
-        maxResults: 8,
+        maxResults: 15,
     };
     $.getJSON(YOUTUBE_SEARCH_URL, parameters, function (data) {
         populateResults(data);
-        console.log(data);
+        // console.log(data);
     });
 }
 
+// function handleImages(search) {
+//     let parameters = {
+//         key: `AIzaSyCrVDceP1-KwRsIVi12ODPCwS2oSHe-_7k`,
+//         q: search,
+//         num: 16,
+//         start: 1,
+//         imgSize: `medium`,
+//         searchType: `image`,
+//         fileType: `jpg`,
+//         cx: `006695032219573469833:ady2qaeevru`,
+//     };
+//     const GOOGLE_SEARCH_URL = 'https://www.googleapis.com/customsearch/v1';
+//     $.getJSON(GOOGLE_SEARCH_URL, parameters, function (data) {
+//         populateResults(data);
+//         console.log(data);
+//     });
+// }
+
 function handleMeetups (search, location) {
-    $.ajax({
+const settings = {
         url: 'https://api.meetup.com/find/groups',
         data: {
             key: '2c4239331347a569527f52571c263c',
             location: location,
             text: search,
             upcoming_events: true,
-            radius: 20
+            radius: 20,
+
         },
         dataType: 'jsonp',
         type: 'GET',
         success: function (data) {
-            console.log(data);
+            // console.log(data);
             populateResultsB(data.data);
         },
         error: function (error) {
             console.log(error);
         }
-    });
+    };
+    $.ajax(settings);
 }
